404a0752750a185808c06e5dc437be911370ba8c
StepanRTL <zasorinctepan@yandex.ru>
2025-04-26 15:00:53 +0300

    Fix(reorder_buffer/rtl): Добавлено рабочее описание модуля reorder_buffer

--- Yadro_Impulse/reorder_buffer/rtl/reorder_buffer.sv
+++ Yadro_Impulse/reorder_buffer/rtl/reorder_buffer.sv
@@ -21,56 +21,73 @@ module reorder_buffer #( parameter DATA_WIDTH = 8) (
   output logic                  m_rready_o
 );
 
-  logic       push1;
-  logic       pop1;
-  logic       empty_fifo1;
-  logic       empty_fifo2;
-  logic [3:0] compare_id1;
-  logic       id_match;
+  logic                      push;
+  logic                      pop;
+  logic                      m_r_handshake;
+  logic                      m_ar_handshake;
+  logic                      s_r_handshake;
+  logic                      empty_fifo;
+  logic                      full_fifo;
+  logic [3:0]                compare_id;
+  logic                      id_match1;
+  logic                      id_match2;
+  logic [DATA_WIDTH - 1 : 0] data_to_ram;
+  logic [DATA_WIDTH - 1 : 0] ram_for_data [15:0];
+  logic [15:0]               register_of_valid_data_in_ram;
 
-  assign id_match = compare_id1 == m_rid_i;
+  assign m_r_handshake  = m_rvalid_i && m_rready_o;
+  assign m_ar_handshake = m_arvalid_o && m_arready_i;
+  assign id_match1      = (compare_id == m_rid_i) && m_r_handshake;
+  assign id_match2      = register_of_valid_data_in_ram[compare_id] == 1'b1;
+  assign pop            = ~empty_fifo && (id_match1 || id_match2) ? 1'b1 : 1'b0;
+  assign push           = s_arvalid_i && s_arready_o;
+  assign data_to_ram    = {m_rdata_i, m_rid_i};
+  assign s_r_handshake  = s_rvalid_o && s_rready_i;
+  assign s_rvalid_o     = register_of_valid_data_in_ram[compare_id] || m_rvalid_i;
+
+  show_ahead_fifo #(.WIDTH(4), .DEPTH(16)) show_ahead_fifo_for_id
+  (
+    .clk_i  (clk       ),
+    .rstn_i (rst_n     ),
+    .push_i (push      ),
+    .pop_i  (pop       ),
+    .data_i (s_arid_i  ),
+    .data_o (compare_id),
+    .empty_o(empty_fifo),
+    .full_o (full_fifo )
+  );
 
   always_ff @(posedge clk) begin
-    if (~rst_n) begin
-      s_rvalid_o <= 1'b0;
-      s_rdata_o  <= 'b0;
-      s_rid_o    <= 'b0;
+    if (~rst_n)
+      register_of_valid_data_in_ram              <= 16'b0;
+    else if (~id_match1 && m_r_handshake) 
+      register_of_valid_data_in_ram[m_rid_i]     <= 1'b1;
+    else if (id_match2)
+      register_of_valid_data_in_ram[compare_id]  <= 1'b0;
+  end
+
+  always_ff @(posedge clk)
+    if (~id_match1 && m_r_handshake)
+      ram_for_data[m_rid_i][DATA_WIDTH + 3 : 0] <= data_to_ram;
+
+  always_comb  begin
+    if (id_match2 && s_r_handshake) begin
+      s_rid_o    = compare_id;
+      s_rdata_o  = ram_for_data[compare_id];
     end
-    else if (id_match && ~empty_fifo1) begin
-      s_rvalid_o <= m_rvalid_i;
-      s_rid_o    <= m_rid_i;
-      s_rdata_o  <= m_rdata_i;
+    else if (id_match1 && s_r_handshake) begin
+      s_rid_o    = m_rid_i;
+      s_rdata_o  = m_rdata_i;
+    end
+    else begin
+      s_rid_o    = 4'b0;
+      s_rdata_o  = 'b0;
     end
   end
 
-
-  assign push1 = s_arvalid_i && s_arready_o;
-  show_ahead_fifo #(.WIDTH(4), .DEPTH(16)) show_ahead_fifo_for_id
-  (
-    .clk_i  (clk        ),
-    .rstn_i (rst_n      ),
-    .push_i (push1      ),
-    .pop_i  (pop1       ),
-    .data_i (s_arid_i   ),
-    .data_o (compare_id1),
-    .empty_o(empty_fifo1),
-    .full_o (full1      )
-  );
-
-  show_ahead_fifo #(.WIDTH(DATA_WIDTH + 4), .DEPTH(16)) show_ahead_fifo_for_id_and_data
-  (
-    .clk_i  (clk              ),
-    .rstn_i (rst_n            ),
-    .push_i (push2            ),
-    .pop_i  (pop2             ),
-    .data_i (data_for_fifo2_ff),
-    .data_o (data_from_fifo2  ),
-    .empty_o(empty_fifo2      ),
-    .full_o (full2            )
-  );
-
-  assign m_arid_o    = s_arid_i;
+  assign m_arid_o    = m_ar_handshake ? s_arid_i : 4'b0;
   assign m_arvalid_o = s_arvalid_i;
-  assign m_rready_o  = m_rid_i == compare_id1;
+  assign s_arready_o = ~full_fifo;
+  assign m_rready_o  = ~(&register_of_valid_data_in_ram);
 
 endmodule
